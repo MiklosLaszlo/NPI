805e8838d6e3a49c52d1a3fa3cdd468a001690b8
Darrukou <juanma.cruzblazquez@gmail.com>
2022-10-07 12:48:30 +0200

    Horario sin terminar

--- script_horario.js
+++ script_horario.js
@@ -1,24 +1,30 @@
 function busca_horario(asignatura) {
-	horario = require('./horarios.json')
-	let salir = false;
-	let index = 0;
-	for (let i = 0; i < horario.length && !salir; i++) {
+	var horario = [{
+		nombre: ["calculo", "cea"],
+		dias: ["martes"],
+		horario: "de nueve a once"
+	}]
+	var salir = false;
+	var index = 0;
+	var i = 0;
+	for (i = 0; i < horario.length && !salir; i++) {
 		salir = (horario[i].nombre.includes(asignatura))
 		if (salir) index = i;
 	}
-	l = horario[index].dias.length;
-	var salida_dias = ""
+	var l = horario[index].dias.length;
+	var salida_dias = "";
 	if (l > 1) {
-		for (let i = 0; i < l; i++) {
-			dias += horario[index].dias[i];
+		for (i = 0; i < l; i++) {
+			salida_dias += horario[index].dias[i];
 			if (i < l - 1)
-				dias += ", ";
+				salida_dias += ", ";
 			else if (i == l - 1)
-				dias += " y ";
+				salida_dias += " y ";
 		}
 	}
+	else
+		salida_dias = horario[index].dias[0];
 
-	var salida_horas = horario[index].horario
-
-	return { dias: salida_dias, horas: salida_horas }
+	var salida_horas = horario[index].horario;
+	return { dias: salida_dias, horas: salida_horas };
 }
