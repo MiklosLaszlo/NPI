fa77e70b2821b3287f6717b486297221d1d3d653
Darrukou <juanma.cruzblazquez@gmail.com>
2022-10-08 13:02:16 +0200

    Update script

---
+++ Funciona_script/script/horarios_ES.js
@@ -0,0 +1,28 @@
+var HORARIO =
+	[
+		{
+			nombre: ["calculo", "cea"],
+			dias: ["martes"],
+			horario: "de nueve a once"
+		},
+		{
+			nombre: ["fundamentos de programacion", "fft"],
+			dias: ["martes", "miercoles"],
+			horario: "de ocho a nueve"
+		},
+		{
+			nombre: ["algebra", "algebra lineal y estructuras matematicas", "alem"],
+			dias: ["miercoles", "viernes"],
+			horario: "de diez a once"
+		},
+		{
+			nombre: ["fundamentos de software", "efe ese"],
+			dias: ["lunes", "martes"],
+			horario: "de nueve a diez"
+		},
+		{
+			nombre: ["fundamentos fisicos y tencologicos", "efe efe te"],
+			dias: ["martes", "jueves"],
+			horario: "de nueve a diez"
+		}
+	];
