dcdac1b4fe2fce46bbfe19b8726f3149082b1f7f
MiklosLaszlo <mikykunsagi@gmail.com>
2022-12-08 12:47:51 +0100

    Ligeros cambios al Layout de navegacion

--- NPI_P3/app/src/main/java/com/example/drawerappugr/Navegacion.java
+++ NPI_P3/app/src/main/java/com/example/drawerappugr/Navegacion.java
@@ -1,58 +1,81 @@
 package com.example.drawerappugr;
 
-import android.app.Activity;
-import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+import org.w3c.dom.Text;
 
 public class Navegacion {
 
     private Spinner spinnerOrigen;
     private Spinner spinnerDestino;
     private Cursor cursor;
+
     private Button scannerButton;
     private Button initNavButton;
     private Button nextNode;
     private Button prevNode;
+    private Button cancelNavButton;
+
     private String origen = new String();
     private String destino = new String();
+    private TextView textInstrucionesPaso;
+    private TextView textOrigen;
+    private TextView textDestino;
+
     private View viewCursor;
-    private View layoutBotones;
+    private View viewOpNav;
+
     private ActivityResultLauncher<ScanOptions> barLauncher;
 
     public Navegacion(AppCompatActivity activity){
+        // Opciones del spinner (menu desplegable)
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(activity, R.array.lugares, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        // Conecto cada elemento con su layout
         spinnerOrigen = (Spinner) activity.findViewById(R.id.menuNavegacionOrigen);
         spinnerDestino = (Spinner) activity.findViewById(R.id.menuNavegacionDestino);
         scannerButton = (Button) activity.findViewById(R.id.scanbuttonav);
         initNavButton = (Button) activity.findViewById(R.id.inicarNav);
-        viewCursor = (View) activity.findViewById(R.id.layoiutCursor);
-        layoutBotones = (View) activity.findViewById(R.id.pasosNav);
+        cancelNavButton = (Button) activity.findViewById(R.id.cancelarNavegacion);
         prevNode = (Button) activity.findViewById(R.id.PrevNode);
         nextNode = (Button) activity.findViewById(R.id.NextNode);
 
+        textInstrucionesPaso = (TextView) activity.findViewById(R.id.tvDestino);
+        textOrigen = (TextView) activity.findViewById(R.id.textOrigen);
+        textDestino = (TextView) activity.findViewById(R.id.textDestino);
 
+        viewCursor = (View) activity.findViewById(R.id.viewCursor);
+        viewOpNav = (View) activity.findViewById(R.id.viewOpcionesNav);
+
+        // Destino y Origen por defecto
+        origen="Entrada";
+        destino="Entrada";
+        textOrigen.setText(origen);
+        textDestino.setText(destino);
+
+        // Creo el menu desplegable
         spinnerDestino.setAdapter(adapter);
         spinnerOrigen.setAdapter(adapter);
 
-
+        // Si eligen algo cargo el origen y el destino
         spinnerOrigen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 origen = parent.getItemAtPosition(position).toString();
-                Log.e("Origen nav",origen);
+                textOrigen.setText(origen);
             }
 
             @Override
@@ -65,7 +88,7 @@ public class Navegacion {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 destino = parent.getItemAtPosition(position).toString();
-                Log.e("Origen nav",destino);
+                textDestino.setText(destino);
             }
 
             @Override
@@ -74,27 +97,49 @@ public class Navegacion {
             }
         });
 
+        // Inicia la flecha
         cursor= new Cursor(activity);
+
+        // Para el scanner, que hace al detectar un QR
         barLauncher = activity.registerForActivityResult(new ScanContract(), result->{
             if(result.getContents() != null){
-                // GUARDA EL TEXTO LEIOD AQUI
+                // GUARDA EL TEXTO LEIDO AQUI
                 origen=result.getContents();
+                textOrigen.setText(origen);
             }
         });
+
+        // Boton inicia escaner
         scannerButton.setOnClickListener(v->{
             scanCode();
         });
 
+        // Boton inicia navegacion
         initNavButton.setOnClickListener(v ->{
             initNav();
         });
+
+        // Boton anterior paso Navegación
         prevNode.setOnClickListener(v->{
             //
         });
+
+        // Boton siguiente paso Navegacion
         nextNode.setOnClickListener(v->{
             //
         });
 
+        // Boton cancelar navegacion
+        cancelNavButton.setOnClickListener(v -> {
+            cancelNav();
+        });
+
+
+    }
+
+    private void cancelNav() {
+        viewOpNav.setVisibility(View.VISIBLE);
+        viewCursor.setVisibility(View.GONE);
 
     }
 
@@ -119,6 +164,7 @@ public class Navegacion {
     }
 
     private void initNav(){
+        viewOpNav.setVisibility(View.GONE);
         viewCursor.setVisibility(View.VISIBLE);
         // Llamar funciones para iniciar navegación
     }
